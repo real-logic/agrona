58181fcd48367a3a61bad37a06a12e58372e8e1e
Martin Thompson <mjpt777@gmail.com>
2015-04-21 18:47:04 +0100

    [Java]: Remove level of indirection with AtomicArray.

--- src/main/java/uk/co/real_logic/agrona/concurrent/AtomicArray.java
+++ src/main/java/uk/co/real_logic/agrona/concurrent/AtomicArray.java
@@ -19,7 +19,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.function.ToIntFunction;
@@ -27,12 +26,12 @@ import java.util.function.ToIntFunction;
 /**
  * A dynamic array that can be used concurrently with a single writer and many readers. All operations are lock-free.
  */
+@SuppressWarnings("unchecked")
 public class AtomicArray<T> implements Collection<T>
 {
     private static final Object[] EMPTY_ARRAY = new Object[0];
 
-    @SuppressWarnings("unchecked")
-    private final AtomicReference<T[]> arrayRef = new AtomicReference<>((T[])EMPTY_ARRAY);
+    private volatile Object[] arrayRef = EMPTY_ARRAY;
 
     @FunctionalInterface
     public interface ToIntLimitedFunction<T>
@@ -55,7 +54,7 @@ public class AtomicArray<T> implements Collection<T>
      */
     public T get(final int index)
     {
-        return arrayRef.get()[index];
+        return (T)arrayRef[index];
     }
 
     /**
@@ -66,7 +65,7 @@ public class AtomicArray<T> implements Collection<T>
      */
     public T findFirst(final Predicate<T> function)
     {
-        final T[] array = arrayRef.get();
+        final T[] array = (T[])arrayRef;
 
         for (final T e : array)
         {
@@ -88,7 +87,7 @@ public class AtomicArray<T> implements Collection<T>
      */
     public void forEach(final Consumer<? super T> action)
     {
-        final T[] array = arrayRef.get();
+        final T[] array = (T[])arrayRef;
 
         for (final T e : array)
         {
@@ -126,7 +125,7 @@ public class AtomicArray<T> implements Collection<T>
      */
     public int doAction(int fromIndex, final ToIntFunction<? super T> action)
     {
-        final T[] array = arrayRef.get();
+        final T[] array = (T[])arrayRef;
         final int length = array.length;
         if (length == 0)
         {
@@ -164,7 +163,7 @@ public class AtomicArray<T> implements Collection<T>
      */
     public int doLimitedAction(int fromIndex, final int actionCountLimit, final ToIntLimitedFunction<? super T> action)
     {
-        final T[] array = arrayRef.get();
+        final T[] array = (T[])arrayRef;
         final int length = array.length;
         if (length == 0)
         {
@@ -206,10 +205,7 @@ public class AtomicArray<T> implements Collection<T>
             throw new NullPointerException("element cannot be null");
         }
 
-        final Object[] oldArray = arrayRef.get();
-        final Object[] newArray = append(oldArray, element);
-
-        arrayRef(newArray);
+        arrayRef = append(arrayRef, element);
 
         return true;
     }
@@ -232,11 +228,10 @@ public class AtomicArray<T> implements Collection<T>
      */
     public T remove(final Predicate<T> predicate)
     {
-        final T[] oldArray = arrayRef.get();
+        final T[] oldArray = (T[])arrayRef;
 
-        if (oldArray == null || oldArray == EMPTY_ARRAY)
+        if (oldArray == EMPTY_ARRAY)
         {
-            arrayRef(EMPTY_ARRAY);
             return null;
         }
 
@@ -245,7 +240,7 @@ public class AtomicArray<T> implements Collection<T>
             final T element = oldArray[0];
             if (predicate.test(element))
             {
-                arrayRef(EMPTY_ARRAY);
+                arrayRef = EMPTY_ARRAY;
                 return element;
             }
             else
@@ -264,24 +259,18 @@ public class AtomicArray<T> implements Collection<T>
         System.arraycopy(oldArray, 0, newArray, 0, index);
         System.arraycopy(oldArray, index + 1, newArray, index, newArray.length - index);
 
-        arrayRef(newArray);
+        arrayRef = newArray;
         return oldArray[index];
-    }
-
-    @SuppressWarnings("unchecked")
-    private void arrayRef(final Object[] emptyArray)
-    {
-        arrayRef.lazySet((T[]) emptyArray);
     }
 
     public int size()
     {
-        return arrayRef.get().length;
+        return arrayRef.length;
     }
 
     public Iterator<T> iterator()
     {
-        return new ArrayIterator<>(arrayRef.get());
+        return new ArrayIterator<>((T[])arrayRef);
     }
 
     private static final class ArrayIterator<T> implements Iterator<T>
@@ -312,29 +301,29 @@ public class AtomicArray<T> implements Collection<T>
 
     public boolean isEmpty()
     {
-        return arrayRef.get().length == 0;
+        return arrayRef.length == 0;
     }
 
     public boolean contains(final Object o)
     {
-        return -1 != find(arrayRef.get(), o);
+        return -1 != find((T[])arrayRef, o);
     }
 
     public Object[] toArray()
     {
-        final Object[] theArray = arrayRef.get();
+        final Object[] theArray = arrayRef;
         return Arrays.copyOf(theArray, theArray.length);
     }
 
     public void clear()
     {
-        arrayRef(EMPTY_ARRAY);
+        arrayRef = EMPTY_ARRAY;
     }
 
     public String toString()
     {
         return "AtomicArray{" +
-            "arrayRef=" + Arrays.toString(arrayRef.get()) +
+            "arrayRef=" + Arrays.toString(arrayRef) +
             '}';
     }
 
@@ -366,15 +355,14 @@ public class AtomicArray<T> implements Collection<T>
         return -1;
     }
 
-    private static Object[] append(final Object[] oldArray, final Object newElement)
+    private static Object[] append(final Object[] array, final Object newElement)
     {
-        final Object[] newArray = Arrays.copyOf(oldArray, oldArray.length + 1);
-        newArray[oldArray.length] = newElement;
+        final Object[] newArray = Arrays.copyOf(array, array.length + 1);
+        newArray[array.length] = newElement;
 
         return newArray;
     }
 
-    @SuppressWarnings("unchecked")
     public <E> E[] toArray(final E[] a)
     {
         return (E[]) toArray();
