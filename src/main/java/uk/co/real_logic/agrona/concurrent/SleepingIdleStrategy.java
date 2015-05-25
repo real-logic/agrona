df694776172c77ba22158136159e62ebaefea906
mjpt777 <mjpt777@gmail.com>
2015-05-25 16:41:07 +0100

    [Java] Moved package of SleepingIdleStrategy and added ManagedResource.

--- src/main/java/uk/co/real_logic/agrona/concurrent/SleepingIdleStrategy.java
+++ src/main/java/uk/co/real_logic/agrona/concurrent/SleepingIdleStrategy.java
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.co.real_logic.agrona;
-
-import uk.co.real_logic.agrona.concurrent.IdleStrategy;
+package uk.co.real_logic.agrona.concurrent;
 
 import java.util.concurrent.locks.LockSupport;
 
