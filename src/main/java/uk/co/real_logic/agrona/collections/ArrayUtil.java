4ca5d1ffe491d1bfb13151d53ab6f594ecea534f
Martin Thompson <mjpt777@gmail.com>
2015-07-30 16:40:09 +0100

    [Java]: Added Javadoc and handled case of remove element not present.

--- src/main/java/uk/co/real_logic/agrona/collections/ArrayUtil.java
+++ src/main/java/uk/co/real_logic/agrona/collections/ArrayUtil.java
@@ -20,26 +20,44 @@ import java.util.Arrays;
 
 /**
  * Utility class for operating on arrays as if they were collections. This is useful for
- * critical paths where operations like add and remove are seldomly used, but iterating
- * is common and checkcast comparatively expensive.
+ * critical paths where operations like add and remove are seldom used, but iterating
+ * is common and checkcast and indirection are comparatively expensive.
  *
  * In all cases the array being mutated is assumed to be full.
  */
 public final class ArrayUtil
 {
-
+    /**
+     * Add an element to an array resulting in a new array.
+     *
+     * @param oldElements  to have the new element added.
+     * @param elementToAdd for the new array.
+     * @param <T>          type of the array.
+     * @return a new array that is one bigger and containing the new element at the end.
+     */
     public static <T> T[] add(final T[] oldElements, final T elementToAdd)
     {
         final int length = oldElements.length;
         final T[] newElements = Arrays.copyOf(oldElements, length + 1);
         newElements[length] = elementToAdd;
+
         return newElements;
     }
 
+    /**
+     * Remove an element from an array resulting in a new array if the element was found otherwise the old array.
+     *
+     * @param oldElements     to have the element removed from.
+     * @param elementToRemove being searched for by identity semantics.
+     * @param <T>             type of the array.
+     * @return a new array without the element if found otherwise the original array.
+     */
     public static <T> T[] remove(final T[] oldElements, final T elementToRemove)
     {
         final int length = oldElements.length;
         final T[] newElements = newArray(oldElements, length);
+        boolean found = false;
+
         for (int i = 0, j = 0; i < length; i++)
         {
             final T element = oldElements[i];
@@ -47,18 +65,26 @@ public final class ArrayUtil
             {
                 newElements[j++] = element;
             }
+            else
+            {
+                found = true;
+            }
         }
 
-        return newElements;
+        return found ? newElements : oldElements;
     }
 
     /**
      * Allocate a new array of the same type as the old array
+     *
+     * @param oldElements on which the new array is based.
+     * @param length      of the new array.
+     * @param <T>         type of the array.
+     * @return            the new array of requested length.
      */
     @SuppressWarnings("unchecked")
     public static <T> T[] newArray(final T[] oldElements, final int length)
     {
-        return (T[]) Array.newInstance(oldElements.getClass().getComponentType(), length - 1);
+        return (T[])Array.newInstance(oldElements.getClass().getComponentType(), length - 1);
     }
-
 }
