1caf1ab2744f3833b186b8322abba37bfb92d618
Martin Thompson <mjpt777@gmail.com>
2016-04-09 10:48:01 +0100

    [Java] Adopt style of Agrona project.

--- src/main/java/org/agrona/hints/ThreadHints.java
+++ src/main/java/org/agrona/hints/ThreadHints.java
@@ -18,7 +18,8 @@ package org.agrona.hints;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
+
+import static java.lang.invoke.MethodType.methodType;
 
 /**
  * This class captures possible hints that may be used by some
@@ -27,40 +28,50 @@ import java.lang.invoke.MethodType;
  * java.lang.Thread class in some Java SE versions, but missing in prior
  * versions.
  */
-public final class ThreadHints {
+public final class ThreadHints
+{
+    private static final MethodHandle ON_SPIN_WAIT_METHOD_HANDLE;
 
-    private static final MethodHandle onSpinWaitMH;
+    static
+    {
+        final MethodHandles.Lookup lookup = MethodHandles.lookup();
 
-    static {
-        MethodHandle mh;
-        MethodHandles.Lookup lookup = MethodHandles.lookup();
-        try {
-            mh = lookup.findStatic(java.lang.Thread.class, "onSpinWait", MethodType.methodType(void.class));
-        } catch (Exception e) {
-            mh = null;
+        MethodHandle methodHandle = null;
+        try
+        {
+            methodHandle = lookup.findStatic(Thread.class, "onSpinWait", methodType(void.class));
         }
-        onSpinWaitMH = mh;
+        catch (final Exception ignore)
+        {
+        }
+
+        ON_SPIN_WAIT_METHOD_HANDLE = methodHandle;
     }
 
-    // prevent construction...
-    private ThreadHints() {
+    private ThreadHints()
+    {
     }
 
-    /** Indicates that the caller is momentarily unable to progress, until the
+    /**
+     * Indicates that the caller is momentarily unable to progress, until the
      * occurrence of one or more actions on the part of other activities.  By
      * invoking this method within each iteration of a spin-wait loop construct,
      * the calling thread indicates to the runtime that it is busy-waiting. The runtime
      * may take action to improve the performance of invoking spin-wait loop
      * constructions.
      */
-    public static void onSpinWait() {
-        // Call java.lang.Runtime.onSpinWait() on Java SE versions that support it. Do nothing otherwise.
-        // This should optimize away to either nothing or to an inlining of java.lang.Runtime.onSpinWait()
-        if (onSpinWaitMH != null) {
-            try {
-                onSpinWaitMH.invokeExact();
-            } catch (Throwable throwable) {
-                // Nothing to do here...
+    public static void onSpinWait()
+    {
+        // Call java.lang.Thread.onSpinWait() on Java SE versions that support it. Do nothing otherwise.
+        // This should optimize away to either nothing or to an inlining of java.lang.Thread.onSpinWait()
+        if (null != ON_SPIN_WAIT_METHOD_HANDLE)
+        {
+            try
+            {
+                ON_SPIN_WAIT_METHOD_HANDLE.invokeExact();
+            }
+            catch (final Throwable ignore)
+            {
             }
         }
     }
