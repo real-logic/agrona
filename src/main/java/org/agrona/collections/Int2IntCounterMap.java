a7151cada4c732362bbd6060e71bc0d8ba049065
nitsanw <nitsanw@yahoo.com>
2017-01-13 16:35:13 +0200

    [Java] Create dedicated counter map to avoid confusing conflation of considerations

---
+++ src/main/java/org/agrona/collections/Int2IntCounterMap.java
@@ -0,0 +1,542 @@
+/*
+ *  Copyright 2014 - 2016 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.agrona.collections;
+
+import org.agrona.BitUtil;
+import org.agrona.generation.DoNotSub;
+
+import java.util.*;
+import java.util.function.IntUnaryOperator;
+
+import static org.agrona.collections.CollectionUtil.validateLoadFactor;
+
+/**
+ * A open addressing with linear probing hash map specialised for primitive key and counter pairs. A counter map views
+ * counters which hit {@link #initialValue} as deleted. This means that changing a counter may impact {@link #size()}.
+ */
+public class Int2IntCounterMap
+{
+    @DoNotSub private final float loadFactor;
+    private final int initialValue;
+    @DoNotSub private int resizeThreshold;
+    @DoNotSub private int size = 0;
+
+    private int[] entries;
+
+
+    public Int2IntCounterMap(final int initialValue)
+    {
+        this(8, 0.67f, initialValue);
+    }
+
+    @SuppressWarnings("unchecked")
+    public Int2IntCounterMap(
+        @DoNotSub final int initialCapacity,
+        @DoNotSub final float loadFactor,
+        final int initialValue)
+    {
+        validateLoadFactor(loadFactor);
+
+        this.loadFactor = loadFactor;
+        this.initialValue = initialValue;
+
+        capacity(BitUtil.findNextPositivePowerOfTwo(initialCapacity));
+    }
+
+    /**
+     * The value to be used as a null marker in the map.
+     *
+     * @return value to be used as a null marker in the map.
+     */
+    public int initialValue()
+    {
+        return initialValue;
+    }
+
+    /**
+     * Get the load factor applied for resize operations.
+     *
+     * @return the load factor applied for resize operations.
+     */
+    public float loadFactor()
+    {
+        return loadFactor;
+    }
+
+    /**
+     * @return map size, counters at {@link #initialValue()} are not counted
+     */
+    @DoNotSub public int size()
+    {
+        return size;
+    }
+
+    /**
+     * @return size == 0
+     */
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+
+    /**
+     * @param key lookup key
+     * @return counter value associated with key, or initialValue if none found
+     */
+    public int get(final int key)
+    {
+        final int[] entries = this.entries;
+        final int initialValue = this.initialValue;
+        @DoNotSub final int mask = entries.length - 1;
+        @DoNotSub int index = Hashing.evenHash(key, mask);
+
+        int value = initialValue;
+        while (entries[index + 1] != initialValue)
+        {
+            if (entries[index] == key)
+            {
+                value = entries[index + 1];
+                break;
+            }
+
+            index = next(index, mask);
+        }
+
+        return value;
+    }
+
+    /**
+     * @param key lookup key
+     * @param value new value, must not be initialValue
+     * @return current counter value associated with key, or initialValue if none found
+     * @throws IllegalArgumentException if value is initialValue
+     */
+    public int put(final int key, final int value)
+    {
+        if (value == initialValue)
+        {
+            throw new IllegalArgumentException("Cannot accept initialValue");
+        }
+        final int[] entries = this.entries;
+        final int initialValue = this.initialValue;
+        @DoNotSub final int mask = entries.length - 1;
+        @DoNotSub int index = Hashing.evenHash(key, mask);
+        int oldValue = initialValue;
+
+        while (entries[index + 1] != initialValue)
+        {
+            if (entries[index] == key)
+            {
+                oldValue = entries[index + 1];
+                break;
+            }
+
+            index = next(index, mask);
+        }
+
+        if (oldValue == initialValue)
+        {
+            ++size;
+            entries[index] = key;
+        }
+
+        entries[index + 1] = value;
+
+        increaseCapacity();
+
+        return oldValue;
+    }
+
+    /**
+     * Convenience version of {@link #addAndGet(int, int)} (key, 1).
+     *
+     * @param key for the counter.
+     * @return the new value.
+     */
+    public int incrementAndGet(final int key)
+    {
+        return addAndGet(key, 1);
+    }
+
+    /**
+     * Convenience version of {@link #addAndGet(int, int)} (key, -1).
+     *
+     * @param key for the counter.
+     * @return the new value.
+     */
+    public int decrementAndGet(final int key)
+    {
+        return addAndGet(key, -1);
+    }
+
+    /**
+     * Add amount to the current value associated with this key. If no such value exists use {@link #initialValue()} as
+     * current value and associate key with {@link #initialValue()} + amount unless amount is 0, in which case map
+     * remains unchanged.
+     *
+     * @param key    new or existing
+     * @param amount to be added
+     * @return the new value associated with the specified key, or
+     *         {@link #initialValue()} + amount if there was no mapping for the key.
+     */
+    public int addAndGet(final int key, int amount)
+    {
+        return getAndAdd(key, amount) + amount;
+    }
+
+    /**
+     * Convenience version of {@link #getAndAdd(int, int)} (key, 1).
+     *
+     * @param key for the counter.
+     * @return the old value.
+     */
+    public int getAndIncrement(final int key)
+    {
+        return getAndAdd(key, 1);
+    }
+
+    /**
+     * Convenience version of {@link #getAndAdd(int, int)} (key, -1).
+     *
+     * @param key for the counter.
+     * @return the old value.
+     */
+    public int getAndDecrement(final int key)
+    {
+        return getAndAdd(key, -1);
+    }
+    /**
+     * Add amount to the current value associated with this key. If no such value exists use {@link #initialValue()} as
+     * current value and associate key with {@link #initialValue()} + amount unless amount is 0, in which case map
+     * remains unchanged.
+     *
+     * @param key    new or existing
+     * @param amount to be added
+     * @return the previous value associated with the specified key, or
+     *         {@link #initialValue()} if there was no mapping for the key.
+     */
+    public int getAndAdd(final int key, int amount)
+    {
+        final int[] entries = this.entries;
+        final int initialValue = this.initialValue;
+        @DoNotSub final int mask = entries.length - 1;
+        @DoNotSub int index = Hashing.evenHash(key, mask);
+        int oldValue = initialValue;
+
+        while (entries[index + 1] != initialValue)
+        {
+            if (entries[index] == key)
+            {
+                oldValue = entries[index + 1];
+                break;
+            }
+
+            index = next(index, mask);
+        }
+
+        if (amount != 0)
+        {
+            final int newValue = oldValue + amount;
+            entries[index + 1] = newValue;
+
+            if (oldValue == initialValue)
+            {
+                ++size;
+                entries[index] = key;
+                increaseCapacity();
+            }
+            else if (newValue == initialValue)
+            {
+                size--;
+                compactChain(index);
+            }
+        }
+        return oldValue;
+    }
+
+    private void increaseCapacity()
+    {
+        if (size > resizeThreshold)
+        {
+            // entries.length = 2 * capacity
+            @DoNotSub final int newCapacity = entries.length;
+            rehash(newCapacity);
+        }
+    }
+
+    private void rehash(@DoNotSub final int newCapacity)
+    {
+        final int[] oldEntries = entries;
+        final int initialValue = this.initialValue;
+        @DoNotSub final int length = entries.length;
+
+        capacity(newCapacity);
+
+        for (@DoNotSub int i = 0; i < length; i += 2)
+        {
+            final int key = oldEntries[i];
+            if (oldEntries[i + 1] != initialValue)
+            {
+                put(key, oldEntries[i + 1]);
+            }
+        }
+    }
+
+    /**
+     * @param consumer a callback called for each key/value pair in the map.
+     */
+    public void forEach(final IntIntConsumer consumer)
+    {
+        final int[] entries = this.entries;
+        final int initialValue = this.initialValue;
+        @DoNotSub final int length = entries.length;
+
+        for (@DoNotSub int i = 0; i < length; i += 2)
+        {
+            if (entries[i + 1] != initialValue)
+            {
+                consumer.accept(entries[i], entries[i + 1]);
+            }
+        }
+    }
+
+    /**
+     * @param key the key to check.
+     * @return true if the map contains key as a mapped key, false otherwise.
+     */
+    public boolean containsKey(final int key)
+    {
+        return get(key) != initialValue;
+    }
+
+    /**
+     * @param value the key to check.
+     * @return true if the map contains value as a mapped value, false otherwise.
+     */
+    public boolean containsValue(final int value)
+    {
+        final int[] entries = this.entries;
+        @DoNotSub final int length = entries.length;
+
+        boolean found = false;
+        for (@DoNotSub int i = 1; i < length; i += 2)
+        {
+            if (value == entries[i])
+            {
+                found = true;
+                break;
+            }
+        }
+
+        return found;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void clear()
+    {
+        Arrays.fill(entries, initialValue);
+        size = 0;
+    }
+
+    /**
+     * Compact the backing arrays by rehashing with a capacity just larger than current size
+     * and giving consideration to the load factor.
+     */
+    public void compact()
+    {
+        @DoNotSub final int idealCapacity = (int)Math.round(size() * (1.0d / loadFactor));
+        rehash(BitUtil.findNextPositivePowerOfTwo(idealCapacity));
+    }
+
+    /**
+     * @param key             to search on.
+     * @param mappingFunction to provide a value if the get returns null.
+     * @return the value if found otherwise the missing value.
+     */
+    public int computeIfAbsent(final int key, final IntUnaryOperator mappingFunction)
+    {
+        int value = get(key);
+        if (value == initialValue)
+        {
+            value = mappingFunction.applyAsInt(key);
+            if (value != initialValue)
+            {
+                put(key, value);
+            }
+        }
+
+        return value;
+    }
+
+    /**
+     * @param key to be removed
+     * @return old value for key
+     */
+    public int remove(final int key)
+    {
+        final int[] entries = this.entries;
+        final int initialValue = this.initialValue;
+        @DoNotSub final int mask = entries.length - 1;
+        @DoNotSub int index = Hashing.evenHash(key, mask);
+
+        int oldValue = initialValue;
+        int candidateKey;
+        while ((candidateKey = entries[index]) != initialValue)
+        {
+            if (candidateKey == key)
+            {
+                @DoNotSub final int valueIndex = index + 1;
+                oldValue = entries[valueIndex];
+                entries[valueIndex] = initialValue;
+                size--;
+
+                compactChain(index);
+
+                break;
+            }
+
+            index = next(index, mask);
+        }
+
+        return oldValue;
+    }
+
+    private void compactChain(@DoNotSub int deleteKeyIndex)
+    {
+        final int[] entries = this.entries;
+        final int initialValue = this.initialValue;
+        @DoNotSub final int mask = entries.length - 1;
+        @DoNotSub int index = deleteKeyIndex;
+
+        while (true)
+        {
+            index = next(index, mask);
+            if (entries[index + 1] == initialValue)
+            {
+                break;
+            }
+
+            @DoNotSub final int hash = Hashing.evenHash(entries[index], mask);
+
+            if ((index < hash && (hash <= deleteKeyIndex || deleteKeyIndex <= index)) ||
+                (hash <= deleteKeyIndex && deleteKeyIndex <= index))
+            {
+                entries[deleteKeyIndex] = entries[index];
+                entries[deleteKeyIndex + 1] = entries[index + 1];
+                entries[index + 1] = initialValue;
+                deleteKeyIndex = index;
+            }
+        }
+    }
+
+    /**
+     * Get the minimum value stored in the map. If the map is empty then it will return {@link #initialValue()}
+     *
+     * @return the minimum value stored in the map.
+     */
+    public int minValue()
+    {
+        final int initialValue = this.initialValue;
+        int min = size == 0 ? initialValue : Integer.MAX_VALUE;
+
+        final int[] entries = this.entries;
+        @DoNotSub final int length = entries.length;
+
+        for (@DoNotSub int i = 1; i < length; i += 2)
+        {
+            final int value = entries[i];
+            if (value != initialValue)
+            {
+                min = Math.min(min, value);
+            }
+        }
+
+        return min;
+    }
+
+    /**
+     * Get the maximum value stored in the map. If the map is empty then it will return {@link #initialValue()}
+     *
+     * @return the maximum value stored in the map.
+     */
+    public int maxValue()
+    {
+        final int initialValue = this.initialValue;
+        int max = size == 0 ? initialValue : Integer.MIN_VALUE;
+
+        final int[] entries = this.entries;
+        @DoNotSub final int length = entries.length;
+
+        for (@DoNotSub int i = 1; i < length; i += 2)
+        {
+            final int value = entries[i];
+            if (value != initialValue)
+            {
+                max = Math.max(max, value);
+            }
+        }
+
+        return max;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String toString()
+    {
+        final StringBuilder sb = new StringBuilder();
+        sb.append('{');
+        forEach(
+            (k, v) ->
+            {
+                sb.append(k);
+                sb.append('=');
+                sb.append(v);
+                sb.append(", ");
+            }
+        );
+
+        if (sb.length() > 1)
+        {
+            sb.setLength(sb.length() - 2);
+        }
+
+        sb.append('}');
+
+        return sb.toString();
+    }
+
+    @DoNotSub private static int next(final int index, final int mask)
+    {
+        return (index + 2) & mask;
+    }
+
+    private void capacity(@DoNotSub int newCapacity)
+    {
+        @DoNotSub final int entriesLength = newCapacity * 2;
+        if (entriesLength < 0)
+        {
+            throw new IllegalStateException("Max capacity reached at size=" + size);
+        }
+
+        /*@DoNotSub*/ resizeThreshold = (int)(newCapacity * loadFactor);
+        entries = new int[entriesLength];
+        size = 0;
+        Arrays.fill(entries, initialValue);
+    }
+}
---
+++ src/test/java/org/agrona/collections/Int2IntCounterMapTest.java
@@ -0,0 +1,431 @@
+/*
+ *  Copyright 2014 - 2016 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.agrona.collections;
+
+import org.junit.Test;
+import org.mockito.InOrder;
+
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.IntUnaryOperator;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+
+public class Int2IntCounterMapTest
+{
+    public static final int INITIAL_VALUE = 0;
+
+    private Int2IntCounterMap map = new Int2IntCounterMap(INITIAL_VALUE);
+
+    @Test
+    public void shouldInitiallyBeEmpty()
+    {
+        assertEquals(0, map.size());
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void getShouldReturninitialValueWhenEmpty()
+    {
+        assertEquals(INITIAL_VALUE, map.get(1));
+    }
+
+    @Test
+    public void getShouldReturninitialValueWhenThereIsNoElement()
+    {
+        map.put(1, 1);
+
+        assertEquals(INITIAL_VALUE, map.get(2));
+    }
+
+    @Test
+    public void getShouldReturnPutValues()
+    {
+        map.put(1, 1);
+
+        assertEquals(1, map.get(1));
+    }
+
+    @Test
+    public void putShouldReturnOldValue()
+    {
+        map.put(1, 1);
+
+        assertEquals(1, map.put(1, 2));
+    }
+
+    @Test
+    public void clearShouldResetSize()
+    {
+        map.put(1, 1);
+        map.put(100, 100);
+
+        map.clear();
+
+        assertEquals(0, map.size());
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void clearShouldRemoveValues()
+    {
+        map.put(1, 1);
+        map.put(100, 100);
+
+        map.clear();
+
+        assertEquals(INITIAL_VALUE, map.get(1));
+        assertEquals(INITIAL_VALUE, map.get(100));
+    }
+
+    @Test
+    public void forEachShouldLoopOverEveryElement()
+    {
+        map.put(1, 1);
+        map.put(100, 100);
+
+        final IntIntConsumer mockConsumer = mock(IntIntConsumer.class);
+        map.forEach(mockConsumer);
+
+        final InOrder inOrder = inOrder(mockConsumer);
+        inOrder.verify(mockConsumer).accept(1, 1);
+        inOrder.verify(mockConsumer).accept(100, 100);
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void shouldNotContainKeyOfAMissingKey()
+    {
+        assertFalse(map.containsKey(1));
+    }
+
+    @Test
+    public void shouldContainKeyOfAPresentKey()
+    {
+        map.put(1, 1);
+
+        assertTrue(map.containsKey(1));
+    }
+
+    @Test
+    public void shouldNotContainValueForAMissingEntry()
+    {
+        assertFalse(map.containsValue(1));
+    }
+
+    @Test
+    public void shouldContainValueForAPresentEntry()
+    {
+        map.put(1, 1);
+
+        assertTrue(map.containsValue(1));
+    }
+
+    @Test
+    public void removeShouldReturnMissing()
+    {
+        assertEquals(INITIAL_VALUE, map.remove(1));
+    }
+
+    @Test
+    public void removeShouldReturnValueRemoved()
+    {
+        map.put(1, 2);
+
+        assertEquals(2, map.remove(1));
+    }
+
+    @Test
+    public void removeShouldRemoveEntry()
+    {
+        map.put(1, 2);
+
+        map.remove(1);
+
+        assertTrue(map.isEmpty());
+        assertFalse(map.containsKey(1));
+        assertFalse(map.containsValue(2));
+    }
+
+    @Test
+    public void shouldOnlyRemoveTheSpecifiedEntry()
+    {
+        IntStream.range(1, 8).forEach((i) -> map.put(i, i * 2));
+
+        map.remove(5);
+
+        IntStream
+            .range(1, 8)
+            .filter((i) -> i != 5L)
+            .forEach(
+                (i) ->
+                {
+                    assertTrue(map.containsKey(i));
+                    assertTrue(map.containsValue(2 * i));
+                });
+    }
+
+    @Test
+    public void shouldResizeWhenMoreElementsAreAdded()
+    {
+        IntStream
+            .range(1, 100)
+            .forEach(
+                (key) ->
+                {
+                    final int value = key * 2;
+                    assertEquals(INITIAL_VALUE, map.put(key, value));
+                    assertEquals(value, map.get(key));
+                });
+    }
+
+    @Test
+    public void shouldHaveNoMinValueForEmptyCollection()
+    {
+        assertEquals(INITIAL_VALUE, map.minValue());
+    }
+
+    @Test
+    public void shouldFindMinValue()
+    {
+        map.put(1, 2);
+        map.put(2, 10);
+        map.put(3, -5);
+
+        assertEquals(-5, map.minValue());
+    }
+
+    @Test
+    public void shouldHaveNoMaxValueForEmptyCollection()
+    {
+        assertEquals(INITIAL_VALUE, map.maxValue());
+    }
+
+    @Test
+    public void shouldFindMaxValue()
+    {
+        map.put(1, 2);
+        map.put(2, 10);
+        map.put(3, -5);
+
+        assertEquals(10, map.maxValue());
+    }
+
+    @Test
+    public void sizeShouldReturnNumberOfEntries()
+    {
+        final int count = 100;
+        for (int key = 0; key < count; key++)
+        {
+            map.put(key, 1);
+        }
+
+        assertEquals(count, map.size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotSupportLoadFactorOfGreaterThanOne()
+    {
+        new Int2IntHashMap(4, 2, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotSupportLoadFactorOfOne()
+    {
+        new Int2IntHashMap(4, 1, 0);
+    }
+
+    @Test
+    public void correctSizeAfterRehash() throws Exception
+    {
+        final Int2IntHashMap map = new Int2IntHashMap(16, 0.6f, -1);
+
+        IntStream.range(1, 17).forEach(i -> map.put(i, i));
+        assertEquals("Map has correct size", 16, map.size());
+
+        final List<Integer> keys = new ArrayList<>(map.keySet());
+        keys.forEach(map::remove);
+
+        assertTrue("Map isn't empty", map.isEmpty());
+    }
+
+    @Test
+    public void shouldComputeIfAbsent()
+    {
+        final int testKey = 7;
+        final int testValue = 7;
+
+        final IntUnaryOperator function = (i) -> testValue;
+
+        assertEquals(map.initialValue(), map.get(testKey));
+
+        assertThat(map.computeIfAbsent(testKey, function), is(testValue));
+        assertThat(map.get(testKey), is(testValue));
+    }
+
+    private void assertEntryIs(final Entry<Integer, Integer> entry, final int expectedKey, final int expectedValue)
+    {
+        assertEquals(expectedKey, entry.getKey().intValue());
+        assertEquals(expectedValue, entry.getValue().intValue());
+    }
+
+    private void assertCollectionContainsElements(final Collection<Integer> keys)
+    {
+        assertEquals(2, keys.size());
+        assertFalse(keys.isEmpty());
+        assertTrue(keys.contains(1));
+        assertTrue(keys.contains(2));
+        assertFalse(keys.contains(3));
+        assertThat(keys, hasItems(1, 2));
+
+        assertThat("iterator has failed to be reset", keys, hasItems(1, 2));
+    }
+
+    private void assertContains(final Iterator<Integer> it, final int first, final int second)
+    {
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(first), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(second), it.next());
+        assertFalse(it.hasNext());
+    }
+
+    private void addTwoElements()
+    {
+        map.put(1, 1);
+        map.put(2, 3);
+    }
+
+    @Test
+    public void shouldContainValueForIncAndDecEntries()
+    {
+        map.incrementAndGet(1);
+        map.getAndIncrement(2);
+        map.getAndAdd(3, 2);
+        map.addAndGet(4, 3);
+        map.decrementAndGet(5);
+        map.getAndDecrement(6);
+
+        assertTrue(map.containsKey(1));
+        assertTrue(map.containsKey(2));
+        assertTrue(map.containsKey(3));
+        assertTrue(map.containsKey(4));
+        assertTrue(map.containsKey(5));
+        assertTrue(map.containsKey(6));
+    }
+
+    @Test
+    public void shouldResultInEmptyAfterIncAndDecWhenEmpty()
+    {
+        map.incrementAndGet(1);
+        map.decrementAndGet(1);
+
+        assertTrue(map.isEmpty());
+
+        map.getAndIncrement(1);
+        map.getAndDecrement(1);
+
+        assertTrue(map.isEmpty());
+
+        map.incrementAndGet(1);
+        map.getAndDecrement(1);
+
+        assertTrue(map.isEmpty());
+
+        map.getAndIncrement(1);
+        map.decrementAndGet(1);
+
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void shouldResultInNotEmptyAfterIncAndDecWhenHaveKey()
+    {
+        map.put(1, 1);
+
+        map.incrementAndGet(1);
+        map.decrementAndGet(1);
+
+        assertEquals(1, map.get(1));
+        assertEquals(1, map.size());
+    }
+
+
+
+    @Test
+    public void shouldReturnInitialValueForGetAndAdd0IfKeyMissing()
+    {
+        final int val = map.getAndAdd(1, 0);
+
+        assertEquals(INITIAL_VALUE, val);
+        assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnOldValueForGetAndAdd0IfKeyExists()
+    {
+        map.put(1, 1);
+        final int val = map.getAndAdd(1, 0);
+
+        assertEquals(1, val);
+        assertEquals(1, map.size());
+    }
+
+    @Test
+    public void shouldReturnOldValueForGetAndAddNot0IfKeyExists()
+    {
+        map.put(1, 1);
+        int amount;
+        do
+        {
+            amount = ThreadLocalRandom.current().nextInt();
+        } while (amount == 0);
+        final int val = map.getAndAdd(1, amount);
+
+        assertEquals(1, val);
+        assertEquals(1, map.size());
+    }
+
+    @Test
+    public void shouldRemoveEntryAfterDecToInitialVal()
+    {
+        map.put(1, INITIAL_VALUE + 1);
+
+        map.decrementAndGet(1);
+
+        assertEquals(INITIAL_VALUE, map.get(1));
+        assertTrue(map.isEmpty());
+        assertFalse(map.containsKey(1));
+    }
+
+    @Test
+    public void shouldRemoveEntryAfterIncToInitialVal()
+    {
+        map.put(1, INITIAL_VALUE - 1);
+
+        map.incrementAndGet(1);
+
+        assertEquals(INITIAL_VALUE, map.get(1));
+        assertTrue(map.isEmpty());
+        assertFalse(map.containsKey(1));
+    }
+}
