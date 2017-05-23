fd3ffe323bc0d31d829cba01ee13441a986a54bf
Martin Thompson <mjpt777@gmail.com>
2017-05-23 17:20:54 +0100

    [Java] Whitespace.

--- agrona/src/main/java/org/agrona/concurrent/DynamicCompositeAgent.java
+++ agrona/src/main/java/org/agrona/concurrent/DynamicCompositeAgent.java
@@ -66,7 +66,6 @@ public class DynamicCompositeAgent implements Agent
             if (null == agent)
             {
                 throw new NullPointerException("Nulls are not supported");
-
             }
         }
 
