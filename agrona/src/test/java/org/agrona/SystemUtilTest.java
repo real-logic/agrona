b1c38e24bafb16b0aa5231749e64582a8993cc1c
Martin Thompson <mjpt777@gmail.com>
2018-03-09 14:28:48 +0000

    [Java] Update copyright and warnings.

--- agrona/src/test/java/org/agrona/SystemUtilTest.java
+++ agrona/src/test/java/org/agrona/SystemUtilTest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017 Real Logic Ltd.
+ * Copyright 2014-2018 Real Logic Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.agrona;
 
 import org.hamcrest.Matchers;
