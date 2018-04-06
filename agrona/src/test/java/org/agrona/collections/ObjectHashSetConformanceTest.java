07e2674e9ac552ec611d8f8100ee5aeac852c6e9
nitsanw <nitsanw@yahoo.com>
2018-04-06 16:13:22 +0200

    [Java] Make ObjectHashSet pass guava suite
    - Add guava dependency
    - Iterator next should not move iterator if there is no next element
    - toArray into a larger array should terminate copy with null

---
+++ agrona/src/test/java/org/agrona/collections/ObjectHashSetConformanceTest.java
@@ -0,0 +1,57 @@
+package org.agrona.collections;
+
+import com.google.common.collect.testing.SampleElements;
+import com.google.common.collect.testing.SetTestSuiteBuilder;
+import com.google.common.collect.testing.TestSetGenerator;
+import com.google.common.collect.testing.features.CollectionFeature;
+import com.google.common.collect.testing.features.CollectionSize;
+import junit.framework.TestSuite;
+
+import java.util.List;
+import java.util.Set;
+
+public class ObjectHashSetConformanceTest
+{
+    // Generated suite to test conformity to the java.util.Set interface
+    public static TestSuite suite()
+    {
+        return SetTestSuiteBuilder.using(new Generator())
+            .named("ObjectHashSet Tests")
+            .withFeatures(CollectionSize.ANY,
+                CollectionFeature.NON_STANDARD_TOSTRING,
+                CollectionFeature.SUPPORTS_ADD,
+                CollectionFeature.SUPPORTS_REMOVE,
+                CollectionFeature.SUPPORTS_ITERATOR_REMOVE,
+                CollectionFeature.REMOVE_OPERATIONS)
+            .createTestSuite();
+    }
+
+    private static class Generator implements TestSetGenerator<String>
+    {
+        public Set<String> create(final Object... elements)
+        {
+            final ObjectHashSet set = new ObjectHashSet(elements.length * 2, Hashing.DEFAULT_LOAD_FACTOR, false);
+            for (final Object o : elements)
+            {
+                set.add(o);
+            }
+
+            return set;
+        }
+
+        public SampleElements<String> samples()
+        {
+            return new SampleElements<>("Elani", "von", "der", "Schavener", "Heide");
+        }
+
+        public String[] createArray(final int length)
+        {
+            return new String[length];
+        }
+
+        public Iterable<String> order(final List<String> insertionOrder)
+        {
+            return insertionOrder;
+        }
+    }
+}
