d580bce6336105fe8b11458de6cf0b4dc7da3f80
Martin Thompson <mjpt777@gmail.com>
2018-07-26 09:20:41 +0100

    [Java] Clean up after merge of PR #149.

--- agrona/src/test/java/org/agrona/ReferencesTest.java
+++ agrona/src/test/java/org/agrona/ReferencesTest.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2018 Gil Tene
+ * Copyright 2018 Real Logic Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,29 +24,27 @@ import java.math.BigInteger;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class ReferenceHelperTest
+public class ReferencesTest
 {
-
     @Test
     public void validateIsCleared()
     {
-        BigInteger big = new BigInteger("42");
-        final WeakReference<BigInteger> ref = new WeakReference<>(big);
-        assertFalse(ReferenceHelper.isCleared(ref));
-        big = null;
+        BigInteger bigInteger = new BigInteger("42");
+        final WeakReference<BigInteger> ref = new WeakReference<>(bigInteger);
+        assertFalse(References.isCleared(ref));
+        bigInteger = null;
         System.gc();
-        assertTrue(ReferenceHelper.isCleared(ref));
+        assertTrue(References.isCleared(ref));
     }
 
     @Test
-    public void validateIsReferringto()
+    public void validateIsReferringTo()
     {
-        final Long longObject1 = 42L;
-        final Long longObject2 = 43L; // Need different value to make sure it is a different instance...
-        final WeakReference<Long> ref = new WeakReference<>(longObject1);
-        assertTrue(ReferenceHelper.isReferringTo(ref, longObject1));
-        assertFalse(ReferenceHelper.isReferringTo(ref, longObject2));
+        final Long objOne = 42L;
+        final Long objTwo = 43L; // Need different value to make sure it is a different instance...
+        final WeakReference<Long> ref = new WeakReference<>(objOne);
+        assertTrue(References.isReferringTo(ref, objOne));
+        assertFalse(References.isReferringTo(ref, objTwo));
     }
-
 }
 
