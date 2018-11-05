fb24b881f071e4e9a011c1fd2819aa554bc72dad
Martin Thompson <mjpt777@gmail.com>
2018-11-05 17:46:51 +0000

    [Java] Added AsciiSequenceView.

---
+++ agrona/src/main/java/org/agrona/AsciiSequenceView.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2018 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.agrona;
+
+/**
+ * View over a {@link DirectBuffer} which contains an ASCII string for a given range.
+ */
+public class AsciiSequenceView implements CharSequence
+{
+    private DirectBuffer buffer;
+    private int offset;
+    private int length;
+
+    public AsciiSequenceView()
+    {
+    }
+
+    /**
+     * Construct a view over a {@link DirectBuffer} from an offset for a given length..
+     *
+     * @param buffer containing the ASCII sequence.
+     * @param offset at which the ASCII sequence begins.
+     * @param length of the ASCII sequence in bytes.
+     */
+    public AsciiSequenceView(final DirectBuffer buffer, final int offset, final int length)
+    {
+        this.buffer = buffer;
+        this.offset = offset;
+        this.length = length;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int length()
+    {
+        return length;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public char charAt(final int index)
+    {
+        return (char)buffer.getByte(offset + index);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public AsciiSequenceView subSequence(final int start, final int end)
+    {
+        if (start < 0)
+        {
+            throw new StringIndexOutOfBoundsException("start=" + start);
+        }
+
+        if (end > length)
+        {
+            throw new StringIndexOutOfBoundsException("end=" + end);
+        }
+
+        if (end - start < 0)
+        {
+            throw new StringIndexOutOfBoundsException("start=" + start + " end=" + end);
+        }
+
+        return new AsciiSequenceView(buffer, offset + start, end - start);
+    }
+
+    /**
+     * Wrap a range of an existing buffer containing an ASCII sequence.
+     *
+     * @param buffer containing the ASCII sequence.
+     * @param offset at which the ASCII sequence begins.
+     * @param length of the ASCII sequence in bytes.
+     * @return this for a fluent API.
+     */
+    public AsciiSequenceView wrap(final DirectBuffer buffer, final int offset, final int length)
+    {
+        this.buffer = buffer;
+        this.offset = offset;
+        this.length = length;
+
+        return this;
+    }
+
+    /**
+     * Reset the view to null.
+     */
+    public void reset()
+    {
+        this.buffer = null;
+        this.offset = 0;
+        this.length = 0;
+    }
+
+    public void getBytes(final MutableDirectBuffer dstBuffer, final int dstOffset)
+    {
+        dstBuffer.putBytes(dstOffset, this.buffer, offset, length);
+    }
+
+    public String toString()
+    {
+        return buffer.getStringWithoutLengthAscii(offset, length);
+    }
+}
---
+++ agrona/src/test/java/org/agrona/AsciiSequenceViewTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2018 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.agrona;
+
+import org.agrona.concurrent.UnsafeBuffer;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class AsciiSequenceViewTest
+{
+    private static final int INDEX = 2;
+    private final MutableDirectBuffer buffer = new UnsafeBuffer(new byte[128]);
+    private final AsciiSequenceView asciiSequenceView = new AsciiSequenceView();
+
+    @Test
+    public void shouldBeAbleToGetChars()
+    {
+        final String data = "stringy";
+        buffer.putStringWithoutLengthAscii(INDEX, data);
+
+        asciiSequenceView.wrap(buffer, INDEX, data.length());
+
+        assertThat(asciiSequenceView.charAt(0), is('s'));
+        assertThat(asciiSequenceView.charAt(1), is('t'));
+        assertThat(asciiSequenceView.charAt(2), is('r'));
+        assertThat(asciiSequenceView.charAt(3), is('i'));
+        assertThat(asciiSequenceView.charAt(4), is('n'));
+        assertThat(asciiSequenceView.charAt(5), is('g'));
+        assertThat(asciiSequenceView.charAt(6), is('y'));
+    }
+
+    @Test
+    public void shouldToString()
+    {
+        final String data = "a little bit of ascii";
+        buffer.putStringWithoutLengthAscii(INDEX, data);
+
+        asciiSequenceView.wrap(buffer, INDEX, data.length());
+
+        assertThat(asciiSequenceView.toString(), is(data));
+    }
+
+    @Test
+    public void shouldReturnCorrectLength()
+    {
+        final String data = "a little bit of ascii";
+        buffer.putStringWithoutLengthAscii(INDEX, data);
+
+        asciiSequenceView.wrap(buffer, INDEX, data.length());
+
+        assertThat(asciiSequenceView.length(), is(data.length()));
+    }
+
+    @Test
+    public void shouldCopyDataUnderTheView()
+    {
+        final String data = "a little bit of ascii";
+        final int targetBufferOffset = 56;
+        final MutableDirectBuffer targetBuffer = new UnsafeBuffer(new byte[128]);
+        buffer.putStringWithoutLengthAscii(INDEX, data);
+        asciiSequenceView.wrap(buffer, INDEX, data.length());
+
+        asciiSequenceView.getBytes(targetBuffer, targetBufferOffset);
+
+        assertThat(targetBuffer.getStringWithoutLengthAscii(targetBufferOffset, data.length()), is(data));
+    }
+
+    @Test
+    public void shouldSubSequence()
+    {
+        final String data = "a little bit of ascii";
+        buffer.putStringWithoutLengthAscii(INDEX, data);
+
+        asciiSequenceView.wrap(buffer, INDEX, data.length());
+        final AsciiSequenceView subSequenceView = asciiSequenceView.subSequence(2, 8);
+
+        assertThat(subSequenceView.toString(), is("little"));
+    }
+}
