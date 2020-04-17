f0625ac49f16376c57b7bf5e1a626bebfd903376
mjpt777 <mjpt777@gmail.com>
2020-04-17 13:33:01 +0100

    [Java] False sharing protection Java 15 style.

--- agrona/src/main/java/org/agrona/concurrent/ManyToOneConcurrentLinkedQueue.java
+++ agrona/src/main/java/org/agrona/concurrent/ManyToOneConcurrentLinkedQueue.java
@@ -25,7 +25,7 @@ import static org.agrona.UnsafeAccess.UNSAFE;
 /**
  * Pad out a cache line to the left of a tail to prevent false sharing.
  */
-class ManyToOneConcurrentLinkedQueuePadding1
+abstract class ManyToOneConcurrentLinkedQueuePadding1
 {
     protected static final long HEAD_OFFSET;
     protected static final long TAIL_OFFSET;
@@ -61,14 +61,16 @@ class ManyToOneConcurrentLinkedQueuePadding1
         }
     }
 
-    @SuppressWarnings("unused")
-    long p1, p2, p3, p4, p5, p6, p7, p8;
+    byte p000, p001, p002, p003, p004, p005, p006, p007, p008, p009, p010, p011, p012, p013, p014, p015;
+    byte p016, p017, p018, p019, p020, p021, p022, p023, p024, p025, p026, p027, p028, p029, p030, p031;
+    byte p032, p033, p034, p035, p036, p037, p038, p039, p040, p041, p042, p043, p044, p045, p046, p047;
+    byte p048, p049, p050, p051, p052, p053, p054, p055, p056, p057, p058, p059, p060, p061, p062, p063;
 }
 
 /**
  * Value for the tail that is expected to be padded.
  */
-class ManyToOneConcurrentLinkedQueueTail<E> extends ManyToOneConcurrentLinkedQueuePadding1
+abstract class ManyToOneConcurrentLinkedQueueTail<E> extends ManyToOneConcurrentLinkedQueuePadding1
 {
     protected volatile ManyToOneConcurrentLinkedQueue.Node<E> tail;
 }
@@ -76,16 +78,18 @@ class ManyToOneConcurrentLinkedQueueTail<E> extends ManyToOneConcurrentLinkedQue
 /**
  * Pad out a cache line between the tail and the head to prevent false sharing.
  */
-class ManyToOneConcurrentLinkedQueuePadding2<E> extends ManyToOneConcurrentLinkedQueueTail<E>
+abstract class ManyToOneConcurrentLinkedQueuePadding2<E> extends ManyToOneConcurrentLinkedQueueTail<E>
 {
-    @SuppressWarnings("unused")
-    long p1, p2, p3, p4, p5, p6, p7, p8;
+    byte p064, p065, p066, p067, p068, p069, p070, p071, p072, p073, p074, p075, p076, p077, p078, p079;
+    byte p080, p081, p082, p083, p084, p085, p086, p087, p088, p089, p090, p091, p092, p093, p094, p095;
+    byte p096, p097, p098, p099, p100, p101, p102, p103, p104, p105, p106, p107, p108, p109, p110, p111;
+    byte p112, p113, p114, p115, p116, p117, p118, p119, p120, p121, p122, p123, p124, p125, p126, p127;
 }
 
 /**
  * Value for the head that is expected to be padded.
  */
-class ManyToOneConcurrentLinkedQueueHead<E> extends ManyToOneConcurrentLinkedQueuePadding2<E>
+abstract class ManyToOneConcurrentLinkedQueueHead<E> extends ManyToOneConcurrentLinkedQueuePadding2<E>
 {
     protected volatile ManyToOneConcurrentLinkedQueue.Node<E> head;
 }
@@ -107,8 +111,10 @@ class ManyToOneConcurrentLinkedQueueHead<E> extends ManyToOneConcurrentLinkedQue
  */
 public class ManyToOneConcurrentLinkedQueue<E> extends ManyToOneConcurrentLinkedQueueHead<E> implements Queue<E>
 {
-    @SuppressWarnings("unused")
-    long p1, p2, p3, p4, p5, p6, p7, p8;
+    byte p128, p129, p130, p131, p132, p133, p134, p135, p136, p137, p138, p139, p140, p142, p143, p144;
+    byte p145, p146, p147, p148, p149, p150, p151, p152, p153, p154, p155, p156, p157, p158, p159, p160;
+    byte p161, p162, p163, p164, p165, p166, p167, p168, p169, p170, p171, p172, p173, p174, p175, p176;
+    byte p177, p178, p179, p180, p181, p182, p183, p184, p185, p186, p187, p189, p190, p191, p192, p193;
 
     private final Node<E> empty = new Node<>(null);
 
