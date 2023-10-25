b5579236353b3a2008e6d4c22d88e926737c0fd0
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2023-10-25 23:16:23 +0200

    [Java] Suppress warnings about Unsafe APIs being deprecated for removal in JDK 22.

--- agrona/src/main/java/org/agrona/concurrent/MemoryAccess.java
+++ agrona/src/main/java/org/agrona/concurrent/MemoryAccess.java
@@ -15,70 +15,13 @@
  */
 package org.agrona.concurrent;
 
-import org.agrona.LangUtil;
 import org.agrona.UnsafeAccess;
-import sun.misc.Unsafe;
-
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-import java.lang.reflect.InvocationTargetException;
 
 /**
  * Memory access operations which encapsulate the use of Unsafe.
  */
 public final class MemoryAccess
 {
-    private static final MethodHandle ACQUIRE_FENCE;
-    private static final MethodHandle RELEASE_FENCE;
-    private static final MethodHandle FULL_FENCE;
-
-    static
-    {
-        MethodHandle acquireFence = null;
-        MethodHandle releaseFence = null;
-        MethodHandle fullFence = null;
-        final MethodHandles.Lookup lookup = MethodHandles.lookup();
-        final MethodType voidMethod = MethodType.methodType(void.class);
-
-        try
-        {
-            final Class<?> versionClass = Class.forName("java.lang.Runtime$Version"); // since JDK 9
-            final Object version = Runtime.class.getMethod("version").invoke(Runtime.getRuntime());
-            final int majorRelease = (int)versionClass.getMethod("feature").invoke(version);
-            if (majorRelease > 21)
-            {
-                final Class<?> varhandleClass = Class.forName("java.lang.invoke.VarHandle");
-                acquireFence = lookup.findStatic(varhandleClass, "acquireFence", voidMethod);
-                releaseFence = lookup.findStatic(varhandleClass, "releaseFence", voidMethod);
-                fullFence = lookup.findStatic(varhandleClass, "fullFence", voidMethod);
-            }
-        }
-        catch (final ClassNotFoundException | NoSuchMethodException | IllegalAccessException |
-                     InvocationTargetException ignored)
-        {
-        }
-
-        if (null == acquireFence)
-        {
-            try
-            {
-                final Class<? extends Unsafe> unsafeClass = UnsafeAccess.UNSAFE.getClass();
-                acquireFence = lookup.findVirtual(unsafeClass, "loadFence", voidMethod).bindTo(UnsafeAccess.UNSAFE);
-                releaseFence = lookup.findVirtual(unsafeClass, "storeFence", voidMethod).bindTo(UnsafeAccess.UNSAFE);
-                fullFence = lookup.findVirtual(unsafeClass, "fullFence", voidMethod).bindTo(UnsafeAccess.UNSAFE);
-            }
-            catch (final NoSuchMethodException | IllegalAccessException ex)
-            {
-                LangUtil.rethrowUnchecked(ex);
-            }
-        }
-
-        ACQUIRE_FENCE = acquireFence;
-        RELEASE_FENCE = releaseFence;
-        FULL_FENCE = fullFence;
-    }
-
     private MemoryAccess()
     {
     }
@@ -86,45 +29,27 @@ public final class MemoryAccess
     /**
      * Ensures that loads before the fence will not be reordered with loads and stores after the fence.
      */
+    @SuppressWarnings("removal") // Since JDK 22
     public static void acquireFence()
     {
-        try
-        {
-            ACQUIRE_FENCE.invokeExact();
-        }
-        catch (final Throwable t)
-        {
-            LangUtil.rethrowUnchecked(t);
-        }
+        UnsafeAccess.UNSAFE.loadFence();
     }
 
     /**
      * Ensures that loads and stores before the fence will not be reordered with stores after the fence.
      */
+    @SuppressWarnings("removal") // Since JDK 22
     public static void releaseFence()
     {
-        try
-        {
-            RELEASE_FENCE.invokeExact();
-        }
-        catch (final Throwable t)
-        {
-            LangUtil.rethrowUnchecked(t);
-        }
+        UnsafeAccess.UNSAFE.storeFence();
     }
 
     /**
      * Ensures that loads and stores before the fence will not be reordered with loads and stores after the fence.
      */
+    @SuppressWarnings("removal") // Since JDK 22
     public static void fullFence()
     {
-        try
-        {
-            FULL_FENCE.invokeExact();
-        }
-        catch (final Throwable t)
-        {
-            LangUtil.rethrowUnchecked(t);
-        }
+        UnsafeAccess.UNSAFE.fullFence();
     }
 }
