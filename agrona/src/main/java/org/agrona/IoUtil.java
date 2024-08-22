5a140e346630f7ba79b1970e44981359d3c776d4
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2024-08-22 17:10:00 +0200

    [Java] Remove `MappedResizeableBuffer` to avoid having to open internal JDK module (i.e. `--add-opens java.base/sun.nio.ch=ALL-UNNAMED`).

--- agrona/src/main/java/org/agrona/IoUtil.java
+++ agrona/src/main/java/org/agrona/IoUtil.java
@@ -16,13 +16,8 @@
 package org.agrona;
 
 import java.io.File;
-import java.io.FileDescriptor;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
@@ -49,77 +44,6 @@ public final class IoUtil
     private static final int MAP_READ_ONLY = 0;
     private static final int MAP_READ_WRITE = 1;
     private static final int MAP_PRIVATE = 2;
-
-    static class MappingMethods
-    {
-        static final MethodHandle MAP_FILE_DISPATCHER;
-        static final MethodHandle UNMAP_FILE_DISPATCHER;
-        static final Object FILE_DISPATCHER;
-        static final MethodHandle GET_FILE_DESCRIPTOR;
-        static final MethodHandle MAP_WITH_SYNC_ADDRESS;
-        static final MethodHandle UNMAP_ADDRESS;
-
-        static
-        {
-            try
-            {
-                final Class<?> fileChannelClass = Class.forName("sun.nio.ch.FileChannelImpl");
-                final Class<?> fileDispatcherClass = Class.forName("sun.nio.ch.FileDispatcher");
-                MethodHandles.Lookup lookup = MethodHandles.lookup();
-
-                Object fileDispatcher = null;
-                MethodHandle mapFileDispatcher = null;
-                MethodHandle getFD = null;
-                MethodHandle mapWithSyncAddress = null;
-                MethodHandle unmapFileDispatcher = null;
-                MethodHandle unmapAddress = null;
-                try
-                {
-                    // JDK 21+
-                    final Field implLookupField = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
-                    lookup = (MethodHandles.Lookup)UnsafeAccess.UNSAFE.getObject(
-                        MethodHandles.Lookup.class, UnsafeAccess.UNSAFE.staticFieldOffset(implLookupField));
-                    fileDispatcher = lookup.unreflectGetter(fileChannelClass.getDeclaredField("nd")).invoke();
-                    getFD = lookup.unreflectGetter(fileChannelClass.getDeclaredField("fd"));
-                    mapFileDispatcher = lookup.unreflect(fileDispatcherClass.getDeclaredMethod(
-                        "map",
-                        FileDescriptor.class,
-                        int.class,
-                        long.class,
-                        long.class,
-                        boolean.class));
-                    unmapFileDispatcher = lookup.unreflect(
-                        fileDispatcherClass.getDeclaredMethod("unmap", long.class, long.class));
-                }
-                catch (final Throwable ex)
-                {
-                    unmapAddress = lookup.unreflect(getMethod(fileChannelClass, "unmap0", long.class, long.class));
-                    // JDK 17
-                    mapWithSyncAddress = lookup.unreflect(getMethod(
-                        fileChannelClass, "map0", int.class, long.class, long.class, boolean.class));
-                }
-
-                MAP_FILE_DISPATCHER = mapFileDispatcher;
-                UNMAP_FILE_DISPATCHER = unmapFileDispatcher;
-                FILE_DISPATCHER = fileDispatcher;
-                GET_FILE_DESCRIPTOR = getFD;
-                MAP_WITH_SYNC_ADDRESS = mapWithSyncAddress;
-                UNMAP_ADDRESS = unmapAddress;
-            }
-            catch (final Exception ex)
-            {
-                throw new RuntimeException(ex);
-            }
-        }
-
-        private static Method getMethod(
-            final Class<?> klass, final String name, final Class<?>... parameterTypes) throws NoSuchMethodException
-        {
-            final Method method = klass.getDeclaredMethod(name, parameterTypes);
-            method.setAccessible(true);
-            return method;
-        }
-    }
 
     private IoUtil()
     {
@@ -541,66 +465,6 @@ public final class IoUtil
     }
 
     /**
-     * Map a range of a file and return the address at which the range begins.
-     *
-     * @param fileChannel to be mapped.
-     * @param mode        for the mapped region.
-     * @param offset      within the file the mapped region should start.
-     * @param length      of the mapped region.
-     * @return the address at which the mapping starts.
-     */
-    public static long map(
-        final FileChannel fileChannel, final FileChannel.MapMode mode, final long offset, final long length)
-    {
-        try
-        {
-            if (null != MappingMethods.MAP_FILE_DISPATCHER)
-            {
-                final FileDescriptor fd = (FileDescriptor)MappingMethods.GET_FILE_DESCRIPTOR.invoke(fileChannel);
-                return (long)MappingMethods.MAP_FILE_DISPATCHER.invoke(
-                    MappingMethods.FILE_DISPATCHER, fd, getMode(mode), offset, length, false);
-            }
-            else
-            {
-                return (long)MappingMethods.MAP_WITH_SYNC_ADDRESS.invoke(
-                    fileChannel, getMode(mode), offset, length, false);
-            }
-        }
-        catch (final Throwable ex)
-        {
-            LangUtil.rethrowUnchecked(ex);
-        }
-
-        return 0;
-    }
-
-    /**
-     * Unmap a region of a file.
-     *
-     * @param fileChannel which has been mapped.
-     * @param address     at which the mapping begins.
-     * @param length      of the mapped region.
-     */
-    public static void unmap(final FileChannel fileChannel, final long address, final long length)
-    {
-        try
-        {
-            if (null != MappingMethods.UNMAP_FILE_DISPATCHER)
-            {
-                MappingMethods.UNMAP_FILE_DISPATCHER.invoke(MappingMethods.FILE_DISPATCHER, address, length);
-            }
-            else
-            {
-                MappingMethods.UNMAP_ADDRESS.invoke(address, length);
-            }
-        }
-        catch (final Throwable ex)
-        {
-            LangUtil.rethrowUnchecked(ex);
-        }
-    }
-
-    /**
      * Unmap a {@link ByteBuffer} without waiting for the next GC cycle if its memory mapped.
      *
      * @param buffer to be unmapped.
@@ -654,21 +518,5 @@ public final class IoUtil
     private static String getFileMode(final FileChannel.MapMode mode)
     {
         return mode == READ_ONLY ? "r" : "rw";
-    }
-
-    private static int getMode(final FileChannel.MapMode mode)
-    {
-        if (mode == READ_ONLY)
-        {
-            return MAP_READ_ONLY;
-        }
-        else if (mode == READ_WRITE)
-        {
-            return MAP_READ_WRITE;
-        }
-        else
-        {
-            return MAP_PRIVATE;
-        }
     }
 }
