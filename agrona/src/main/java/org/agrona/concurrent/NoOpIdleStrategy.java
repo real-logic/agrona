3a696134da6d9e528bb7233158199fd64392f6f6
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2024-11-28 21:27:40 +0100

    [Java] Clarify no op idle strategy docs around sys-calls.

--- agrona/src/main/java/org/agrona/concurrent/NoOpIdleStrategy.java
+++ agrona/src/main/java/org/agrona/concurrent/NoOpIdleStrategy.java
@@ -18,15 +18,18 @@ package org.agrona.concurrent;
 /**
  * Low-latency idle strategy to be employed in loops that do significant work on each iteration such that any
  * work in the idle strategy would be wasteful.
+ *
  * <p>
- * The NoOpIdleStrategy should be used with care:
+ * The {@link NoOpIdleStrategy} should be used with care:
  * <ol>
  *     <li>It could increase power consumption.</li>
  *     <li>The increased power consumption could lead to thermal throttling causing an overall performance drop.</li>
  *     <li>It could consume resources that otherwise would be used by the hyper-sibling.</li>
  *     <li>It could lead to a memory order violation at the end of the loop causing a pipeline reset.</li>
  * </ol>
- * The {@link BusySpinIdleStrategy} might be a better alternative in some scenario's.
+ * The {@link BusySpinIdleStrategy} might be a better alternative in some scenarios. That being said, using
+ * {@link NoOpIdleStrategy} is perfectly fine if the underlying duty cycle loops is performing syscalls (e.g. in sender
+ * and receiver threads in Aeron).
  */
 public final class NoOpIdleStrategy implements IdleStrategy
 {
